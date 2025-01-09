d24542523cb21f079b6da0e391693f503ae822b4
gfonseca92 <gustavofonsecaufmg@gmail.com>
2025-01-09 08:04:55 -0300

    Cavenaghi Interface: code to encapsulate the Cavenaghi implementation into the bandits-lib taxonomy

--- bandits/algorithms/cavenaghi_interface.py
+++ bandits/algorithms/cavenaghi_interface.py
@@ -1,14 +1,33 @@
 from multi_armed_bandit.algorithms.bernoulli_dist.min_dsw_ts import MinDSWTS
+from multi_armed_bandit.algorithms.bernoulli_dist.max_dsw_ts import MaxDSWTS
+from multi_armed_bandit.algorithms.bernoulli_dist.mean_dsw_ts import MeanDSWTS
+from typing import Literal
+
 from .bandit import Bandit
 
 
-class CavenaghiMinDSWTS(Bandit):
+class CavenaghiFDSWTS(Bandit):
 
-    _algo: MinDSWTS
+        def __init__(
+                self,
+                f: Literal['min', 'max', 'mean'],
+                n_arms: int,
+                gamma: float = 0.9,
+                n: int = 30,
+                store_estimates: bool = True
+        ):
+            if f == 'min':
+                self._algo = MinDSWTS(n_arms=n_arms, gamma=gamma, n=n, store_estimates=store_estimates)
+            elif f == 'max':
+                self._algo = MaxDSWTS(n_arms=n_arms, gamma=gamma, n=n, store_estimates=store_estimates)
+            elif f == 'mean':
+                self._algo = MeanDSWTS(n_arms=n_arms, gamma=gamma, n=n, store_estimates=store_estimates)
+            else:
+                raise ValueError('f must be one of "min", "max", "mean"')
+            super().__init__(n_arms, self._algo)
 
-    def __init__(self, n_arms: int, gamma: float = 0.9, n: int = 30, store_estimates:bool=True):
-        self._algo = MinDSWTS(n_arms=n_arms, gamma=gamma, n=n, store_estimates=store_estimates)
-        super().__init__(n_arms, self._algo)
+        def update(self, chosen_arm, reward, max_reward):
+            self._algo.update_estimates(chosen_arm, reward)
 
-    def update(self, chosen_arm, reward, max_reward):
-        self._algo.update_estimates(chosen_arm, reward)
+        def select_arm(self):
+            return self._algo.select_action()
